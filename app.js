e6b7d42dddfab32ea2360ffbc3fddde9024e68e9
sangga3331 <1552314@hcmut.edu.vn>
2019-09-16 01:13:56 +0700

    add get current ipv4

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var http = require('http');
+var ip = require('ip');
 
+var IPv4Address = ip.address();
 var app = express();
-// var server = http.createServer(app);
 var todoController = require('./controllers/todoController');
 
 //set up template engine
@@ -11,13 +12,10 @@ app.set('view engine', 'ejs');
 //static files
 app.use(express.static('./public'));
 
-// var server = http.Server(app);
-
 //fire controller
 todoController(app);
 
 //listen to port
 console.log('start');
-//put your Ipv4 address here
-app.listen(80, '192.168.1.74');
-console.log("you are now listening on port 80 with http");
+app.listen(80, IPv4Address);
+console.log("you are now listening on port 80 with Ipv4 address "+IPv4Address);
